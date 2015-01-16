dc8827b14f2d48d3a3bb4280b501baee98f56548
jzd-tsai <jzd.tsai@gmail.com>
2015-01-16 17:54:31 +0800

    目錄crud目前需要的完成

--- app/controllers/superadmin/store_categories_controller.rb
+++ app/controllers/superadmin/store_categories_controller.rb
@@ -3,9 +3,19 @@ class Superadmin::StoreCategoriesController < Superadmin::BaseController
 	def new
 		@store_categories = StoreCategory.new
 	end
+	def create
+		@store_category = StoreCategory.new(store_category)
+		if @store_category.save
+			flash[:notice] = "#{@store_category.title}Save"
+			redirect_to superadmin_store_categories_path
+		else
+			flash[:notice] = "#{@store_category.title} Error"
+			redirect_to superadmin_store_categories_path
+		end
+	end
 
 	private
 	def store_category
-		require.params(:store_categories).permit(:title, :anchor)
+		params.require(:store_category).permit(:store_id, :title, :anchor)
 	end
 end
