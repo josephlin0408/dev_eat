fe6d16411899caa377100bcbb7142ad0f84babda
jzd-tsai <jzd.tsai@gmail.com>
2015-01-22 15:23:23 +0800

    form_for 透過url: 解決了商家編輯問題.

--- app/controllers/mystore/settings_controller.rb
+++ app/controllers/mystore/settings_controller.rb
@@ -5,9 +5,17 @@ class Mystore::SettingsController < ApplicationController
 	def edit
 		@setting = Store.find_by(id: params[:id])
 	end
+	def update
+		@setting = Store.find_by(id: params[:id])
+		if @setting.update(mystore_params)
+			redirect_to mystore_settings_path
+		else
+			render :edit
+		end
+	end
 
 	private
 	def mystore_params
-		params.require(:mystore).permit(:name, :user_id, :address, :zip, :tel, :power, :status)
+		params.require(:store).permit(:name, :address, :zip, :tel)
 	end
 end
