56846d730cebf0554e445550cb340b4d07bee32f
jzd-tsai <jzd.tsai@gmail.com>
2015-01-28 10:48:24 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 User.create!(email: 'jzd.tsai@gmail.com', password: '00000000', password_confirmation: '00000000', admin: true)
 User.create!(email: 'test@gmail.com', password: '00000000', password_confirmation: '00000000')
+User.create!(email: 'store@gmail.com', password: '00000000', password_confirmation: '00000000', store: true)
+User.create!(email: 'admin@gmail.com', password: '00000000', password_confirmation: '00000000', admin: true)
